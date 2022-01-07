6a1288ec8a13ddda0be519760ad3126af04190fd
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2022-01-07 17:05:58 +0100

    adding in repo search

--- src/components/Repositories.js
+++ src/components/Repositories.js
@@ -3,19 +3,54 @@ import React, { Component } from 'react';
 export default class Repositories extends Component {
 
     state = {
-        display: null
+        display: null,
+        repoSearch: ''
+
     }
 
     componentDidUpdate(prevProps, prevState) {
         console.log(prevProps)
         if(this.props.avatarUrl !== prevProps.avatarUrl) {
-            console.log("UPDATED")
+            const toDisplay = this.generateDisplay(this.props.repos);
+            this.setState({
+                display: toDisplay
+            })
         }
+        // if(this.state.repoSearch !== prevState.repoSearch && 
+        //     this.state.repoSearch.length > 3 && 
+        //     this.state.display !== null) {
+        //     console.log("Searching current repos")
+        //     const toDisplay = this.generateDisplay([this.props.repos[0]])
+        //     this.setState({
+        //         display: toDisplay
+        //     })
+        // }
+    }
+
+    generateDisplay = (repos) => {
+        return <div>
+            {repos.map((data, key) => {
+                return (
+                    <div key={key}>
+                        <p>{data.name}</p>
+                    </div>
+                )
+            })}
+        </div>
+    }
+
+    updateRepoSearch = (e) => {
+        this.setState({
+            repoSearch: e.target.value
+        })
     }
 
     render() {
         return (
             <div>
+                Repos
+                <input onChange={this.updateRepoSearch}/>
+                <button>Search for Repo</button>
                 {this.state.display}
             </div>
         )
