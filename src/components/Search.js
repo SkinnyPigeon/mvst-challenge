0af2088d37d3e205d355336c9809a5fd0839385b
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2022-01-07 23:15:59 +0100

    starting to add some more styling

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,13 +4,14 @@ import Repositories from './Repositories';
 export default class Search extends Component {
     state = {
         login: "SkinnyPigeon",
+        userName: '',
         avatarUrl: '',
         bio: null,
         company: null,
-        createdAt: '',
         location: '',
         followers: [],
-        repositories: []
+        repositories: [],
+        cannotFindUser: '',
     }
 
     componentDidMount() {
@@ -30,7 +31,6 @@ export default class Search extends Component {
                 url
                 bio
                 company
-                createdAt
                 location
                 followers(first: 100) {
                     nodes {
@@ -63,15 +63,24 @@ export default class Search extends Component {
                 const user = body.data.user;
                 this.setState({
                     avatarUrl: user.avatarUrl,
+                    userName: user.login,
                     bio: user.bio,
                     company: user.company,
-                    createdAt: user.company,
+                    createdAt: user.createdAt,
                     location: user.location,
                     followers: user.followers.nodes,
-                    repositories: user.repositories.nodes
+                    repositories: user.repositories.nodes,
+                    cannotFindUser: '',
                 });
             }) 
-            .catch(error => console.error(error));
+            .catch(error => {
+                console.error(error);
+                if(error) {
+                    this.setState({
+                        cannotFindUser: `Cannot find user with username ${this.state.login}. Please check spelling`
+                    })
+                }
+            });
     }
 
     updateLogin = (e) => {
@@ -81,11 +90,21 @@ export default class Search extends Component {
     }
 
     render() {
+        const notFoundStyle = {
+            color: "#3e3e3e",
+            opacity: "50%"
+        }
         return (
             <div className='main'>
                 <div className='search'>
                     <input placeholder='Enter name of user' onChange={this.updateLogin}/>
                     <button onClick={this.searchUser}>Search for User</button>
+                    <p>{this.state.cannotFindUser}</p>
+                    {this.state.avatarUrl !== '' ? <img className='avatar' src={this.state.avatarUrl} alt="avatar"></img> : <div class="loader">Loading...</div>}
+                    <p>{this.state.userName}</p>
+                    {this.state.bio !== null ? <p>{this.state.bio}</p> : <p style={notFoundStyle}>No bio set</p>}
+                    {this.state.company !== null ? <p>{this.state.company}</p> : <p style={notFoundStyle}>No company set</p>}
+                    {this.state.location !== null ? <p>{this.state.location}</p> : <p style={notFoundStyle}>No location set</p>}
                 </div>
                 <Repositories 
                     repos={this.state.repositories}
