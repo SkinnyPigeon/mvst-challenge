234b7d8e0a57ebbd861274362837a83ca1a6c1ef
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2022-01-08 20:50:41 +0100

    search lifted up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,118 @@
+import React, { Component } from 'react';
+
 import Header from './components/Header';
 import Search from './components/Search';
+import Repositories from './components/Repositories';
 
+export default class App extends Component {
+  state = {
+    login: "SkinnyPigeon",
+    userName: '',
+    avatarUrl: '',
+    bio: null,
+    company: null,
+    location: '',
+    followers: [],
+    repositories: [],
+    cannotFindUser: '',
+    repos: [],
+    display: null,
+    repoSearch: ''
+  }
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Search />
-    </div>
-  );
+  searchUser = () => {
+    this.setState({
+      avatarUrl: ''
+    })
+    const query = `
+    query {
+        user (login:"${this.state.login}") {
+            avatarUrl
+            login
+            url
+            bio
+            company
+            location
+            followers(first: 100) {
+                nodes {
+                    name
+                    url
+                }
+            }
+            repositories(first: 5, orderBy: {field: CREATED_AT, direction: DESC}) {
+                nodes {
+                    name
+                    description
+                    url
+                    createdAt
+                    primaryLanguage {
+                        name
+                        color
+                    }
+                }
+            }
+        }
+    }`;
+    fetch('https://api.github.com/graphql', {
+      method: 'POST',
+      body: JSON.stringify({ query }),
+      headers: {
+        'Authorization': `Bearer ${process.env.REACT_APP_TOKEN}`,
+      },
+    }).then(res => res.json())
+      .then(body => {
+        const user = body.data.user;
+        this.setState({
+          avatarUrl: user.avatarUrl,
+          userName: user.login,
+          bio: user.bio,
+          company: user.company,
+          createdAt: user.createdAt,
+          location: user.location,
+          followers: user.followers.nodes,
+          repositories: user.repositories.nodes,
+          cannotFindUser: '',
+        });
+      })
+      .catch(error => {
+        console.error(error);
+        if (error) {
+          this.setState({
+            cannotFindUser: `Cannot find user with username ${this.state.login}. Please check spelling`
+          })
+        }
+      });
+  }
+
+  updateLogin = (e) => {
+    this.setState({
+      login: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Search
+          searchUser={this.searchUser}
+          updateLogin={this.updateLogin}
+          login={this.state.login}
+          userName={this.state.userName}
+          avatarUrl={this.state.avatarUrl}
+          bio={this.state.bio}
+          company={this.state.company}
+          location={this.state.location}
+          followers={this.state.followers}
+          repositories={this.state.repositories}
+          cannotFindUser={this.state.cannotFindUser}
+        />
+        <Repositories
+          repos={this.state.repositories}
+          avatarUrl={this.state.avatarUrl}
+
+        />
+      </div>
+    )
+  }
 }
-
-export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Repositories from './Repositories';
+// import Repositories from './Repositories';
 
 export default class Search extends Component {
     state = {
@@ -15,81 +15,11 @@ export default class Search extends Component {
     }
 
     componentDidMount() {
-        this.searchUser();
+        this.props.searchUser();
     }
 
     componentDidUpdate() {
-        console.log(this.state);
-    }
-
-    searchUser = () => {
-        this.setState({
-            avatarUrl: ''
-        })
-        const query = `
-        query {
-            user (login:"${this.state.login}") {
-                avatarUrl
-                login
-                url
-                bio
-                company
-                location
-                followers(first: 100) {
-                    nodes {
-                        name
-                        url
-                    }
-                }
-                repositories(first: 5, orderBy: {field: CREATED_AT, direction: DESC}) {
-                    nodes {
-                        name
-                        description
-                        url
-                        createdAt
-                        primaryLanguage {
-                            name
-                            color
-                        }
-                    }
-                }
-            }
-        }`;
-        fetch('https://api.github.com/graphql', {
-            method: 'POST',
-            body: JSON.stringify({query}),
-            headers: {
-                'Authorization': `Bearer ${process.env.REACT_APP_TOKEN}`,
-            },
-            }).then(res => res.json())
-            .then(body => {
-                const user = body.data.user;
-                this.setState({
-                    avatarUrl: user.avatarUrl,
-                    userName: user.login,
-                    bio: user.bio,
-                    company: user.company,
-                    createdAt: user.createdAt,
-                    location: user.location,
-                    followers: user.followers.nodes,
-                    repositories: user.repositories.nodes,
-                    cannotFindUser: '',
-                });
-            }) 
-            .catch(error => {
-                console.error(error);
-                if(error) {
-                    this.setState({
-                        cannotFindUser: `Cannot find user with username ${this.state.login}. Please check spelling`
-                    })
-                }
-            });
-    }
-
-    updateLogin = (e) => {
-        this.setState({
-            login: e.target.value
-        });
+        console.log(this.props);
     }
 
     render() {
@@ -100,19 +30,19 @@ export default class Search extends Component {
         return (
             <div className='main'>
                 <div className='search'>
-                    <input placeholder='Enter name of user' onChange={this.updateLogin} type="text"/>
-                    <button onClick={this.searchUser}>Search for User</button>
-                    <p>{this.state.cannotFindUser}</p>
-                    {this.state.avatarUrl !== '' ? <img className='avatar' src={this.state.avatarUrl} alt="avatar"></img> : <div className="loader">Loading...</div>}
-                    <p>{this.state.userName}</p>
-                    {this.state.bio !== null ? <p>{this.state.bio}</p> : <p style={notFoundStyle}>No bio set</p>}
-                    {this.state.company !== null ? <p>{this.state.company}</p> : <p style={notFoundStyle}>No company set</p>}
-                    {this.state.location !== null ? <p>{this.state.location}</p> : <p style={notFoundStyle}>No location set</p>}
+                    <input placeholder='Enter name of user' onChange={this.props.updateLogin} type="text"/>
+                    <button onClick={this.props.searchUser}>Search for User</button>
+                    <p>{this.props.cannotFindUser}</p>
+                    {this.props.avatarUrl !== '' ? <img className='avatar' src={this.props.avatarUrl} alt="avatar"></img> : <div className="loader">Loading...</div>}
+                    <p>{this.props.userName}</p>
+                    {this.props.bio !== null ? <p>{this.props.bio}</p> : <p style={notFoundStyle}>No bio set</p>}
+                    {this.props.company !== null ? <p>{this.props.company}</p> : <p style={notFoundStyle}>No company set</p>}
+                    {this.props.location !== null ? <p>{this.props.location}</p> : <p style={notFoundStyle}>No location set</p>}
                 </div>
-                <Repositories 
+                {/* <Repositories 
                     repos={this.state.repositories}
                     avatarUrl={this.state.avatarUrl}
-                />
+                /> */}
             </div>
         )
     }
