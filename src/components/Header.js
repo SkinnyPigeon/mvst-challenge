5050d3dadfa088f2d182150f451101bdd0f4decf
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2022-01-08 16:33:38 +0100

    adding header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className='header'>
+                <h1>GitHüb</h1>
+                <div>
+                    
+                </div>
+            </div>
+        )
+    }
+}
