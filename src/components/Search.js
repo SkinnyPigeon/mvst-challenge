4f4c80f3961583c01d6042cd95d648b7833164ec
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2022-01-09 23:42:48 +0100

    bolded user name

--- src/components/Search.js
+++ src/components/Search.js
@@ -33,7 +33,7 @@ export default class Search extends Component {
             <button onClick={this.props.searchUser}>Search for User</button>
             <p>{this.props.cannotFindUser}</p>
             {this.props.avatarUrl !== '' ? <img className='avatar' src={this.props.avatarUrl} alt="avatar"></img> : <div className="loader">Loading...</div>}
-            <p>{this.props.userName}</p>
+            <h3>{this.props.userName}</h3>
             {this.props.bio !== null ? <p>{this.props.bio}</p> : <p style={notFoundStyle}>No bio set</p>}
             {this.props.company !== null ? <p>{this.props.company}</p> : <p style={notFoundStyle}>No company set</p>}
             {this.props.location !== null ? <p>{this.props.location}</p> : <p style={notFoundStyle}>No location set</p>}
